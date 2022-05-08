806b3d81184037d4fc6b8469c0e163d196995540
realelongatedmusket <s10222819@connect.np.edu.sg>
2022-05-09 01:36:02 +0800

    Completed practical 3

---
+++ app/src/main/java/sg/edu/np/mad/test1mad/User.java
@@ -0,0 +1,16 @@
+package sg.edu.np.mad.test1mad;
+
+public class User {
+    String name;
+    String description;
+    int id;
+    boolean followed;
+
+    public User(String Name, String Desc, int Id, boolean Followed){
+        name = Name;
+        description = Desc;
+        id = Id;
+        followed = Followed;
+    }
+
+}
